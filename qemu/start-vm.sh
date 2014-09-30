07f3aa4457e3aebfd08cb25b81be514b6d226876
Yubin Xia <xiayubin@gmail.com>
2014-09-30 19:40:16 +0800

    add network port forwarding in qemu

--- qemu/start-vm.sh
+++ qemu/start-vm.sh
@@ -5,6 +5,7 @@ qemu-system-x86_64 -machine accel=kvm -cpu host -smp 1 -m "${memory}" \
     -drive cache=writeback,if=virtio,file=./ubuntu.img \
     -drive cache=writeback,if=virtio,file=./swap.img \
     -drive cache=writeback,if=virtio,file=./ext4-raw.img \
+    -device e1000,netdev=user.0 -netdev user,id=user.0,hostfwd=tcp::5555-:22 \
     -vnc 0.0.0.0:0
     # -drive cache=writeback,if=virtio,file=/mnt/ramfs/ext4-raw-ramfs.img \
 
